0e5ef3711c826e062f8bf4da3d99736fae2b7c37
LORD-Nikhil-Patil <Nikhil876706@gmail.com>
2025-04-05 08:23:10 +0530

    Auction to bid

--- frontend/src/components/quickListModal/hook.tsx
+++ frontend/src/components/quickListModal/hook.tsx
@@ -7,13 +7,18 @@ import {
   useReadContract,
 } from "wagmi";
 import _ from "lodash";
+
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
 import Marketplace from "@/utils/contracts/Marketplace.json";
 import AdvancedERC1155 from "@/utils/contracts/AdvancedERC1155.json";
 import { PinataFile } from "@/types";
-import { useEffect, useState } from "react";
 import { formattedPercentage } from "@/utils/ethUtils";
 import useHandleFiles from "@/store/fileSlice";
-
+import { reduceEth } from "@/utils/ethUtils";
 interface IUseQuickListingModal {
   file: PinataFile;
   setClose: (value: boolean) => void;
@@ -29,6 +34,31 @@ export const useQuickListingModal = ({
   const publicClient = usePublicClient();
   const [royaltyPercentage, setRoyaltyPercentage] = useState<number>(0);
   const [maxTokenForListing, setMaxTokenForListing] = useState<number>(0);
+
+  const listingFormSchema = z.object({
+    amount: z.coerce
+      .number()
+      .min(1, { message: "Must be at least 1" })
+      .max(maxTokenForListing, {
+        message: `Must be less than ${maxTokenForListing}`,
+      })
+      .refine((val) => val > 0, {
+        message: "amount must be a positive number",
+      }),
+    price: z.coerce
+      .number()
+      .min(0.0000000001, { message: "Must be at least 1" })
+      .refine((val) => val > 0, {
+        message: "Price must be a positive number",
+      }),
+  });
+
+  const quickListingForm = useForm<z.infer<typeof listingFormSchema>>({
+    resolver: zodResolver(listingFormSchema),
+    defaultValues: {
+      amount: 1,
+    },
+  });
 
   const contractAddress = process.env.NEXT_PUBLIC_MARKETPLACE_ADDRESS;
   const { data: currentSupply } = useReadContract({
@@ -58,8 +88,8 @@ export const useQuickListingModal = ({
     }
   }, [data, currentSupply]);
 
-  const handleSetQuickListing = async (price, amount) => {
-    const priceInWei = parseUnits(price.toString(), 18);
+  const handleSetQuickListing = async (data) => {
+    const priceInWei = parseUnits(data.price.toString(), 18);
     if (
       _.isEmpty(file.tokenId) &&
       _.isEmpty(file.tokenAddress) &&
@@ -76,7 +106,7 @@ export const useQuickListingModal = ({
         args: [
           file.tokenAddress,
           Number(file.tokenId),
-          Number(amount),
+          Number(data.amount),
           Number(priceInWei),
         ],
         chainId: chainId,
@@ -90,7 +120,7 @@ export const useQuickListingModal = ({
           if (receipt.status.toLowerCase() === "success") {
             await updateFiles({
               tokenId: file.tokenId,
-              price: price,
+              price: data.price,
             });
             setClose(false);
           }
@@ -105,6 +135,188 @@ export const useQuickListingModal = ({
   return {
     royaltyPercentage,
     maxTokenForListing,
+    quickListingForm,
     handleSetQuickListing,
+  };
+};
+
+interface IUseQuickAuctionModal {
+  file?: PinataFile;
+  setClose?: (value: boolean) => void;
+}
+
+export const useQuickAuctionModal = ({
+  file,
+  setClose,
+}: IUseQuickAuctionModal = {}) => {
+  const { writeContract } = useWriteContract();
+  const { address, chainId, chain } = useAccount();
+  const [endDate, setEndDate] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [earnings, setEarnings] = useState(0);
+  const [maxTokenForListing, setMaxTokenForListing] = useState<number>(0);
+  const [royaltyPercentage, setRoyaltyPercentage] = useState<number>(0);
+  const publicClient = usePublicClient();
+  const auctionFormSchema = z.object({
+    amount: z.coerce
+      .number()
+      .min(1, { message: "Must be at least 1" })
+      .max(maxTokenForListing, {
+        message: `Must be less than ${maxTokenForListing}`,
+      })
+      .refine((val) => val > 0, {
+        message: "amount must be a positive number",
+      }),
+    price: z.coerce
+      .number()
+      .min(0.0000000001, { message: "Must be at least 1" })
+      .refine((val) => val > 0, {
+        message: "Price must be a positive number",
+      }),
+    duration: z.enum(["1 day", "3 days", "1 week", "1 month", "3 months"]),
+  });
+  const calculateEndDateTime = (duration: string) => {
+    const now = new Date();
+    let endDate = new Date(now);
+
+    switch (duration) {
+      case "1 day":
+        endDate.setDate(now.getDate() + 1);
+        break;
+      case "3 days":
+        endDate.setDate(now.getDate() + 3);
+        break;
+      case "1 week":
+        endDate.setDate(now.getDate() + 7);
+        break;
+      case "1 month":
+        endDate.setMonth(now.getMonth() + 1);
+        break;
+      case "3 months":
+        endDate.setMonth(now.getMonth() + 3);
+        break;
+      default:
+        break;
+    }
+
+    const year = endDate.getFullYear();
+    const month = String(endDate.getMonth() + 1).padStart(2, "0");
+    const day = String(endDate.getDate()).padStart(2, "0");
+    const hours = String(endDate.getHours()).padStart(2, "0");
+    const minutes = String(endDate.getMinutes()).padStart(2, "0");
+
+    setEndDate(`${year}-${month}-${day}`);
+    setEndTime(`${hours}:${minutes}`);
+  };
+  const quickAuctionForm = useForm<z.infer<typeof auctionFormSchema>>({
+    resolver: zodResolver(auctionFormSchema),
+    defaultValues: {
+      amount: 1,
+      price: 0,
+      duration: "1 day"
+    },
+  });
+
+  const contractAddress = process.env.NEXT_PUBLIC_MARKETPLACE_ADDRESS;
+
+  const { data: currentSupply } = useReadContract({
+    address: file.tokenAddress as `0x${string}`,
+    abi: AdvancedERC1155.abi,
+    functionName: "totalSupply",
+    args: [Number(file.tokenId)],
+  });
+
+  const { data } = useReadContract({
+    address: file.tokenAddress as `0x${string}`,
+    abi: AdvancedERC1155.abi,
+    functionName: "royaltyInfo",
+    args: [file.tokenId, parseEther("0.01")],
+    query: {
+      enabled: !!file.tokenAddress,
+    },
+  });
+
+  useEffect(() => {
+    if (data) {
+      const royalties = formattedPercentage(data[1]);
+      setRoyaltyPercentage(royalties);
+    }
+    if (currentSupply) {
+      setMaxTokenForListing(Number(currentSupply));
+    }
+  }, [data, currentSupply]);
+
+  useEffect(() => {
+    calculateEndDateTime(quickAuctionForm.getValues("duration"));
+
+    const subscription = quickAuctionForm.watch((value, { name }) => {
+      if (name === "duration" || !name) {
+        calculateEndDateTime(value.duration || "1 day");
+      }
+    });
+    return () => subscription.unsubscribe();
+  }, [quickAuctionForm.watch]);
+
+  useEffect(() => {
+    const calculatedEarnings = reduceEth(
+      royaltyPercentage,
+      Number(quickAuctionForm.watch("price")),
+    );
+    setEarnings(calculatedEarnings);
+  }, [quickAuctionForm.getValues("price")]);
+
+  const handleSetQuickAuction = (data) => {
+    const priceInWei = parseUnits(data.price.toString(), 18);
+    const endTimestamp = Math.floor(
+      new Date(`${endDate}T${endTime}`).getTime() / 1000
+    )
+
+    const currentTimestamp = Math.floor(Date.now() / 1000)
+    const duration = endTimestamp - currentTimestamp;
+    if (duration <= 0) {
+      console.error("End time must be in the future")
+      return;
+    }
+    writeContract(
+      {
+        abi: Marketplace.abi as Abi,
+        account: address,
+        address: contractAddress as `0x${string}`,
+        functionName: "createAuction",
+        args: [
+          file.tokenAddress,
+          Number(file.tokenId),
+          Number(data.amount),
+          Number(priceInWei),
+          duration
+        ],
+        chainId: chainId,
+        chain: chain,
+      },
+      {
+        onSuccess: async (data) => {
+          const receipt = await publicClient.waitForTransactionReceipt({
+            hash: data,
+          });
+          if (receipt.status.toLowerCase() === "success") {
+            setClose(false);
+          }
+        },
+
+        onError: (error) => {
+          console.log("error", error);
+        },
+      },
+    );
+  }
+
+  return {
+    endDate,
+    endTime,
+    earnings,
+    quickAuctionForm,
+    setEndDate,
+    setEndTime,
+    handleSetQuickAuction
   };
 };
--- frontend/src/components/quickListModal/index.tsx
+++ frontend/src/components/quickListModal/index.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
@@ -20,8 +16,7 @@ import Image from "next/image";
 import _ from "lodash";
 import { PinataFile } from "@/types";
 import { Switch } from "@radix-ui/react-switch";
-import { useQuickListingModal } from "./hook";
-import { reduceEth } from "@/utils/ethUtils";
+import { useQuickListingModal, useQuickAuctionModal } from "./hook";
 
 interface QuickListingModalProps {
   setClose: (value: boolean) => void;
@@ -34,109 +29,27 @@ export default function QuickListingModal({
   handleEthToUsd,
   fileForListing,
 }: QuickListingModalProps) {
-  const { royaltyPercentage, maxTokenForListing, handleSetQuickListing } = useQuickListingModal({
+  const {
+    royaltyPercentage,
+    maxTokenForListing,
+    handleSetQuickListing,
+    quickListingForm,
+  } = useQuickListingModal({
     file: fileForListing,
-    setClose
+    setClose,
   });
-
-  const [endDate, setEndDate] = useState("");
-  const [endTime, setEndTime] = useState("");
-  const [earnings, setEarnings] = useState(0);
-
-  const formSchema = z.object({
-    amount: z.coerce
-      .number()
-      .min(1, { message: "Must be at least 1" })
-      .max(maxTokenForListing, {
-        message: `Must be less than ${maxTokenForListing}`,
-      })
-      .refine((val) => val > 0, {
-        message: "amount must be a positive number",
-      }),
-    price: z.coerce
-      .number()
-      .min(0.0000000001, { message: "Must be at least 1" })
-      .refine((val) => val > 0, {
-        message: "Price must be a positive number",
-      }),
-    duration: z.enum(["1 day", "3 days", "1 week", "1 month", "3 months"]),
+  const {
+    earnings,
+    endDate,
+    endTime,
+    quickAuctionForm,
+    setEndDate,
+    setEndTime,
+    handleSetQuickAuction,
+  } = useQuickAuctionModal({
+    file: fileForListing,
+    setClose,
   });
-  const quickListingForm = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      amount: 1,
-      duration: "1 day",
-    },
-  });
-
-  const calculateEndDateTime = (duration: string) => {
-    const now = new Date();
-    let endDate = new Date(now);
-
-    switch (duration) {
-      case "1 day":
-        endDate.setDate(now.getDate() + 1);
-        break;
-      case "3 days":
-        endDate.setDate(now.getDate() + 3);
-        break;
-      case "1 week":
-        endDate.setDate(now.getDate() + 7);
-        break;
-      case "1 month":
-        endDate.setMonth(now.getMonth() + 1);
-        break;
-      case "3 months":
-        endDate.setMonth(now.getMonth() + 3);
-        break;
-      default:
-        break;
-    }
-
-    const year = endDate.getFullYear();
-    const month = String(endDate.getMonth() + 1).padStart(2, "0");
-    const day = String(endDate.getDate()).padStart(2, "0");
-    const hours = String(endDate.getHours()).padStart(2, "0");
-    const minutes = String(endDate.getMinutes()).padStart(2, "0");
-
-    setEndDate(`${year}-${month}-${day}`);
-    setEndTime(`${hours}:${minutes}`);
-  };
-
-  useEffect(() => {
-    calculateEndDateTime(quickListingForm.getValues("duration"));
-
-    const subscription = quickListingForm.watch((value, { name }) => {
-      if (name === "duration" || !name) {
-        calculateEndDateTime(value.duration || "1 day");
-      }
-    });
-    return () => subscription.unsubscribe();
-  }, [quickListingForm.watch]);
-
-  useEffect(() => {
-    const calculatedEarnings = reduceEth(
-      royaltyPercentage,
-      Number(quickListingForm.watch("price")),
-    );
-    setEarnings(calculatedEarnings);
-  }, [quickListingForm.getValues("price")]);
-
-  const onSubmit = (data: z.infer<typeof formSchema>) => {
-    const metadata = {
-      cid: fileForListing.IpfsHash,
-      keyValues: {
-        amount: parseFloat(data.price),
-        duration: data.duration,
-        endDate: endDate,
-        endTime: endTime,
-        ...fileForListing.KeyValues,
-      },
-      name: fileForListing.KeyValues.name,
-    };
-    
-    handleSetQuickListing(data.price, data.amount);
-  };
 
   return (
     <div className="flex items-center justify-center bg-black/80 z-50 min-w-full min-h-full fixed inset-0">
@@ -158,48 +71,51 @@ export default function QuickListingModal({
               </Button>
             </div>
           </div>
-          <div className="flex items-center gap-4 p-4">
-            <div className="relative w-16 h-16 overflow-hidden rounded-lg">
-              {fileForListing.IpfsHash && (
-                <Image
-                  src={`https://silver-rainy-chipmunk-430.mypinata.cloud/ipfs/${fileForListing.AssetIpfsHash}`}
-                  alt="NFT Image"
-                  width={64}
-                  height={64}
-                  className="object-cover bg-blue-600"
-                />
-              )}
-            </div>
-            <div>
-              <h3 className="text-lg font-medium">
-                {fileForListing.KeyValues.name}
-              </h3>
-              <p className="text-sm text-gray-400">based editions</p>
-            </div>
-            <div className="ml-auto text-right">
-              <p className="text-sm text-gray-400">Listing price per item</p>
-              <p className="font-medium">
-                {!_.isEmpty(quickListingForm.getValues("price"))
-                  ? `${quickListingForm.getValues("price")} ETH`
-                  : "-- ETH"}
-              </p>
-              {quickListingForm.watch("price") && (
-                <span className="text-sm text-gray-400">
-                  $
-                  {handleEthToUsd(
-                    Number(quickListingForm.watch("price")),
-                  ).toFixed(2)}{" "}
-                  USD
-                </span>
-              )}
-            </div>
-          </div>
+
           <TabsContent
             value="listing"
             className="h-[410px] w-[550px] overflow-y-auto"
           >
+            <div className="flex items-center gap-4 p-4">
+              <div className="relative w-16 h-16 overflow-hidden rounded-lg">
+                {fileForListing.IpfsHash && (
+                  <Image
+                    src={`https://silver-rainy-chipmunk-430.mypinata.cloud/ipfs/${fileForListing.AssetIpfsHash}`}
+                    alt="NFT Image"
+                    width={64}
+                    height={64}
+                    className="object-cover bg-blue-600"
+                  />
+                )}
+              </div>
+              <div>
+                <h3 className="text-lg font-medium">
+                  {fileForListing.KeyValues.name}
+                </h3>
+                <p className="text-sm text-gray-400">based editions</p>
+              </div>
+              <div className="ml-auto text-right">
+                <p className="text-sm text-gray-400">Listing price per item</p>
+                <p className="font-medium">
+                  {!_.isEmpty(quickListingForm.getValues("price"))
+                    ? `${quickListingForm.getValues("price")} ETH`
+                    : "-- ETH"}
+                </p>
+                {quickListingForm.watch("price") && (
+                  <span className="text-sm text-gray-400">
+                    $
+                    {handleEthToUsd(
+                      Number(quickListingForm.watch("price")),
+                    ).toFixed(2)}{" "}
+                    USD
+                  </span>
+                )}
+              </div>
+            </div>
             <CardContent className="p-0">
-              <form onSubmit={quickListingForm.handleSubmit(onSubmit)}>
+              <form
+                onSubmit={quickListingForm.handleSubmit(handleSetQuickListing)}
+              >
                 <div className="p-4 space-y-6">
                   {/* NFT Item */}
 
@@ -219,9 +135,11 @@ export default function QuickListingModal({
                         <p className="text-red-500 text-sm mt-1">
                           {quickListingForm.formState.errors.amount.message}
                         </p>
-                      ) : <p className="text-sm text-gray-400 ml-auto w-fit">
-                        available: {maxTokenForListing}
-                      </p>}
+                      ) : (
+                        <p className="text-sm text-gray-400 ml-auto w-fit">
+                          available: {maxTokenForListing}
+                        </p>
+                      )}
                     </div>
                     <div className="flex items-center gap-1 mb-2">
                       <h3 className="text-base font-medium">
@@ -324,10 +242,51 @@ export default function QuickListingModal({
           </TabsContent>
           <TabsContent value="auction" className="h-[410px] w-[550px]">
             <CardContent className="p-0 max-h-96 overflow-y-auto">
-              <div className="mx-auto p-4 space-y-6">
+              <div className="flex items-center gap-4 p-4">
+                <div className="relative w-16 h-16 overflow-hidden rounded-lg">
+                  {fileForListing.IpfsHash && (
+                    <Image
+                      src={`https://silver-rainy-chipmunk-430.mypinata.cloud/ipfs/${fileForListing.AssetIpfsHash}`}
+                      alt="NFT Image"
+                      width={64}
+                      height={64}
+                      className="object-cover bg-blue-600"
+                    />
+                  )}
+                </div>
+                <div>
+                  <h3 className="text-lg font-medium">
+                    {fileForListing.KeyValues.name}
+                  </h3>
+                  <p className="text-sm text-gray-400">based editions</p>
+                </div>
+                <div className="ml-auto text-right">
+                  <p className="text-sm text-gray-400">
+                    Listing price per item
+                  </p>
+                  <p className="font-medium">
+                    {!_.isEmpty(quickAuctionForm.getValues("price"))
+                      ? `${quickAuctionForm.getValues("price")} ETH`
+                      : "-- ETH"}
+                  </p>
+                  {quickAuctionForm.watch("price") && (
+                    <span className="text-sm text-gray-400">
+                      $
+                      {handleEthToUsd(
+                        Number(quickAuctionForm.watch("price")),
+                      ).toFixed(2)}{" "}
+                      USD
+                    </span>
+                  )}
+                </div>
+              </div>
+              <form
+                className="mx-auto p-4 space-y-6"
+                onSubmit={quickAuctionForm.handleSubmit(handleSetQuickAuction)}
+              >
                 <div className="space-y-6">
                   {/* Method Selection */}
-                  <div className="space-y-6">
+                  <div className="space-y-1">
                     <div className="flex items-center justify-between">
                       <label className="text-sm font-medium">Method</label>
                       <HelpCircle className="h-4 w-4 text-muted-foreground" />
@@ -346,22 +305,60 @@ export default function QuickListingModal({
                       </SelectContent>
                     </Select>
                   </div>
-
+                  <div className="my-4">
+                    <p className="mb-1 text-base font-medium"># of items</p>
+                    <Input
+                      type="number"
+                      placeholder="Amount"
+                      className="rounded-[15px] bg-[#2a2a2a] border-white focus-visible:ring-0 text-[150px] h-16 min-h-16 my-2"
+                      {...quickAuctionForm.register("amount", {
+                        valueAsNumber: true,
+                      })}
+                    />
+                    {quickAuctionForm.formState.errors.amount ? (
+                      <p className="text-red-500 text-sm mt-1">
+                        {quickAuctionForm.formState.errors.amount.message}
+                      </p>
+                    ) : (
+                      <p className="text-sm text-gray-400 ml-auto w-fit">
+                        available: {maxTokenForListing}
+                      </p>
+                    )}
+                  </div>
                   {/* Starting Price */}
-                  <div className="space-y-6">
-                    <div className="flex items-center justify-between">
-                      <label className="text-sm font-medium">
+                  <div className="space-y-4">
+                    <div>
+                      <p className="mb-2 text-base font-medium">
                         Starting price
-                      </label>
-                    </div>
-                    <div className="relative">
-                      <div className="absolute left-3 top-1/2 -translate-y-1/2 flex items-center text-sm text-muted-foreground">
-                        WETH
+                      </p>
+                      <div className="flex">
+                        <Input
+                          type="number"
+                          step={0.0000000001}
+                          placeholder="Price"
+                          className="rounded-l-[15px] bg-[#2a2a2a] border-white focus-visible:ring-0 text-[150px] h-16 min-h-16"
+                          {...quickAuctionForm.register("price")}
+                        />
+                        <div className="flex items-center justify-center px-4 font-medium bg-[#2a2a2a] border border-l-0 border-white rounded-r-[15px]">
+                          ETH
+                        </div>
                       </div>
-                      <Input className="pl-16" defaultValue="0.04" />
-                    </div>
-                    <div className="text-xs text-right text-muted-foreground">
-                      $17.58 Total
+                      <div className="flex flex-row justify-between">
+                        {quickAuctionForm.formState.errors.price && (
+                          <p className="text-red-500 text-sm mt-1">
+                            {quickAuctionForm.formState.errors.price.message}
+                          </p>
+                        )}
+                        {quickAuctionForm.watch("price") && (
+                          <span className="text-sm text-gray-400 left-auto ml-auto mt-1">
+                            $
+                            {handleEthToUsd(
+                              Number(quickAuctionForm.watch("price")),
+                            ).toFixed(2)}{" "}
+                            USD
+                          </span>
+                        )}
+                      </div>
                     </div>
                   </div>
 
@@ -371,9 +368,9 @@ export default function QuickListingModal({
                     <div className="flex">
                       <div className="mr-2 flex-1">
                         <Select
-                          value={quickListingForm.watch("duration")}
+                          value={quickAuctionForm.watch("duration")}
                           onValueChange={(value) =>
-                            quickListingForm.setValue("duration", value)
+                            quickAuctionForm.setValue("duration", value)
                           }
                         >
                           <SelectTrigger className="bg-[#2a2a2a] border-gray-700 focus:ring-0">
@@ -391,14 +388,15 @@ export default function QuickListingModal({
                       <input
                         type="date"
                         value={endDate}
-                        readOnly
+                        min={new Date().toISOString().split("T")[0]}
                         className="px-3 bg-[#2a2a2a] border-gray-700 outline-none"
+                        onChange={(e) => setEndDate(e.target.value)}
                       />
                       <input
                         type="time"
                         value={endTime}
-                        readOnly
                         className="px-3 bg-[#2a2a2a] border-gray-700 outline-none"
+                        onChange={(e) => setEndTime(e.target.value)}
                       />
                     </div>
                   </div>
@@ -434,10 +432,13 @@ export default function QuickListingModal({
                 </div>
 
                 {/* Complete Listing Button */}
-                <Button className="w-full bg-slate-700 hover:bg-slate-600z text-primary-foreground">
+                <Button
+                  type="submit"
+                  className="w-full bg-slate-700 hover:bg-slate-600z text-primary-foreground"
+                >
                   Complete listing
                 </Button>
-              </div>
+              </form>
             </CardContent>
           </TabsContent>
         </Card>
